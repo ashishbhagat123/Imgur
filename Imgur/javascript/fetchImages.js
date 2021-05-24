38811bfbb9eea528858a245fba462f4efc53004b
Ashish <abhagat629@gmail.com>
2021-05-24 17:36:10 +0530

    added search Image bar

--- Imgur/javascript/fetchImages.js
+++ Imgur/javascript/fetchImages.js
@@ -4,17 +4,23 @@ let input = document.getElementById("search")
 let first = document.getElementById("first_div")
 let second = document.getElementById("second_div")
 let third = document.getElementById("third_div")
+let fourth = document.getElementById("fourth_div")
+let search = document.getElementById("search_button")
 let f = 0
 let s = 1
 let t = 2
+let four = 3
 
+let q = input.value
 let page = 1
 let images = []
 let first_arr = []
 let second_arr = []
 let third_arr = []
+let fourth_arr = []
 
 const createFirstCard = (image,des,likes) => {
+    console.log(des)
     let box = document.createElement("div")
     box.setAttribute("class", "image_box")
     let img = document.createElement("img")
@@ -27,7 +33,6 @@ const createFirstCard = (image,des,likes) => {
     details.appendChild(tag)
     box.appendChild(details)
     first.appendChild(box)
-    console.log(box)
 }
 const createSecondCard = (image,des,likes) => {
     let box = document.createElement("div")
@@ -42,7 +47,6 @@ const createSecondCard = (image,des,likes) => {
     details.appendChild(tag)
     box.appendChild(details)
     second.appendChild(box)
-    console.log(box)
 }
 const createThirdCard = (image,des,likes) => {
     let box = document.createElement("div")
@@ -57,38 +61,57 @@ const createThirdCard = (image,des,likes) => {
     details.appendChild(tag)
     box.appendChild(details)
     third.appendChild(box)
-    console.log(box)
 }
 
+const createFourthCard = (image,des,likes) => {
+    console.log(des)
+    let box = document.createElement("div")
+    box.setAttribute("class", "image_box")
+    let img = document.createElement("img")
+    img.src = image
+    let details = document.createElement("div")
+    details.setAttribute("class", "details")
+    let tag = document.createElement("p")
+    tag.innerHTML = des
+    box.appendChild(img)
+    details.appendChild(tag)
+    box.appendChild(details)
+    fourth.appendChild(box)
+}
 
-const fetchImage = (q, page) => {
+const fetchImage = (page) => {
     if(q.length === 0){
         q = "random"
     }
+    console.log(input.value, images)
     fetch(
         `https://api.unsplash.com/search/photos?client_id=gfOFYkEseSN7KRbhiyU2ckFssPgnSiClx_utWbq-b8k&page=${page || "1"}&query=${q}`,
     )
         .then((response) => response.json())
         .then((result) => {
-            console.log(result)
             let data = result.results
             images = [...images,...data]
             for (i in images){
                 if(i == f){
                     first_arr.push(images[i])
-                    f = f + 3
+                    f = f + 4
                 }
                 if(i == s){
                     second_arr.push(images[i])
-                    s = s + 3
+                    s = s + 4
                 }
                 if(i == t){
                     third_arr.push(images[i])
-                    t = t + 3
+                    t = t + 4
+                }
+                if(i == four){
+                    fourth_arr.push(images[i])
+                    four = four + 4
                 }
             }
             
             for (i in first_arr){
+                console.log("here", first_arr)
                 let image = first_arr[i].urls.small
                 let des = first_arr[i].alt_description
                 let likes = first_arr[i].likes
@@ -101,38 +124,48 @@ const fetchImage = (q, page) => {
                 createSecondCard(image, des, likes)
             }
             for (i in third_arr){
+                
                 let image = third_arr[i].urls.small
                 let des = third_arr[i].alt_description
                 let likes = third_arr[i].likes
                 createThirdCard(image, des, likes)
             }
+            for (i in fourth_arr){
+                console.log("Ii")
+                let image = fourth_arr[i].urls.small
+                let des = fourth_arr[i].alt_description
+                let likes = fourth_arr[i].likes
+                createFourthCard(image, des, likes)
+            }
         })
         .catch((error) => console.log("error", error));
 };
+fetchImage(page)
 
-
-input.addEventListener("keyup",debounce())
-function debounce(fetchImage){ 
+search.addEventListener("click", function(){
+    images = []
     first_arr = []
     second_arr = []
     third_arr = []
-    let timer;
-    return () => {
-        clearTimeout(timer)
-        timer = setTimeout(() => {
-            // fetchImage(input.value,page)
-            console.log(input.value)
-        }, 3000); 
-    }
-}
+    fourth_arr = []
+    f = 0
+    s = 1
+    t = 2
+    four = 3
+    q = input.value
+    first.innerHTML = ""
+    second.innerHTML = ""
+    third.innerHTML = ""
+    fourth.innerHTML = ""
+    fetchImage(page)
+})
 
 
-fetchImage("india", page)
 
 window.addEventListener("scroll", function(){
     if(window.pageYOffset >= document.body.scrollHeight-800)
 		{
-            fetchImage("london", page++)
+            fetchImage(page++)
 		}
 })
 
